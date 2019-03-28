4fa6e798f8f684da8ac52abb4bcf2a51fda9ce5c
James Dindi <dindijjames@gmail.com>
2019-03-28 11:45:14 +0300

    Configure store and redux devtools

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+import reduxImmutabeStateInvariant from 'redux-immutable-state-invariant';
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // Add support for redux devtools
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutabeStateInvariant()))
+  );
+}
