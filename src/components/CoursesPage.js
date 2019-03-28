67cff83d197ad8a98cb43c8ba4a4f7c1d5e197dd
James Dindi <dindijjames@gmail.com>
2019-03-28 05:41:11 +0300

    Add action creator and action

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,23 +1,25 @@
 import React, {Component} from 'react';
 
 class CoursesPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       course: {
         title: '',
       },
-    }
-  }
+    };
 
-  handleChange(event) {
+  handleChange = event => {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
-  }
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title)
+  };
 
   render() {
     return (
-      <form action="">
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
         <input type="text" onChange={this.handleChange} value={this.state.course.title}/>
